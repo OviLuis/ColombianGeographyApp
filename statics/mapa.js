35014e0ef18f6fd30e7a6a50cf9777813bcfed6d
AndreaMora <morita_9313@hotmail.com>
2015-11-28 19:59:13 -0500

    Cntrolador mapa

---
+++ statics/mapa.js
@@ -0,0 +1,10 @@
+var map;
+
+function init(){
+	var mapOptions={
+		center: new google.maps.LatLng(4.6840363,-72.1363275),
+		zoom: 5,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	};
+ 	map= new google.maps.Map(document.getElementById('map'), mapOptions)
+}
