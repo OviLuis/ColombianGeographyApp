734350559d96eb2f03e3549bb0ac3e61035d5bf2
Luis Gonzalez <luis.ovidio.gonzalez@correounivalle.edu.co>
2015-11-28 21:24:57 -0500

    Vista Regiones Naturales

---
+++ tjpzoom/tjpzoom_config_default.js
@@ -0,0 +1,29 @@
+// TJPzoom 3 configuration file * János Pál Tóth
+// 2007.04.28
+// Docs @ http://valid.tjp.hu/tjpzoom/ 
+// News @ http://tjpzoom.blogspot.com/
+
+// DEFAULT
+// a bit of a border (2px)
+// and a bit of a drop shadow
+
+var TJPzoomwidth=200;              //zoom window size
+var TJPzoomheight=200; 
+var TJPzoomwindowlock=0;           //set to 1 to lock window size
+
+var TJPzoomoffsetx=.5;
+var TJPzoomoffsety=.5;
+
+var TJPzoomamount=4;
+var TJPzoomamountmax=12;
+var TJPzoomamountmin=1;
+
+var TJPborderthick=4;              //border thickness, SET 0 to no borders
+var TJPbordercolor='#cccccc';      //bo../rder color
+
+var TJPshadowthick=8;              //shadow image size/2, SET 0 to have no shadows (saves cpu)
+var TJPshadow='../tjpzoom/dropshadow/';       // <>: n, ne, e, se, s, sw, w, nw - TJPshadow+'nw.png'
+
+// TJPzoom 3 configuration file * János Pál Tóth
+// Docs @ http://valid.tjp.hu/tjpzoom/ 
+// News @ http://tjpzoom.blogspot.com/
